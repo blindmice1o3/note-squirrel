754ea022015060ff9a335751f634fb3b3cc598e0
jamesgov <blindmice1o3@gmail.com>
2020-02-16 09:54:46 -0800

    feat: Write EditText's content to internal storage
    Previously, the "save" button was merely displaying predefined text to Logcat as a verification that the OnClickListener was working. Now the "save" button gets a reference to the EditText (View component) instance and writes its content to a file named "note_squirrel.txt" (which is saved to the device's internal storage).

--- app/src/main/java/com/jackingaming/notesquirrel/MainActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/MainActivity.java
@@ -2,14 +2,20 @@ package com.jackingaming.notesquirrel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String DEBUG_TAG = "JJG";
+    public static final String TEXT_FILE = "note_squirrel.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,25 @@ public class MainActivity extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(DEBUG_TAG, "Save button clicked");
+                //Log.d(DEBUG_TAG, "Save button clicked: " + text);
+                    //was used to verify the button was actually "doing stuff"
+                    //when we clicked it (before we supplied implementation).
+
+                EditText editText = (EditText) findViewById(R.id.text);
+
+                String text = editText.getText().toString();
+
+                try {
+                    FileOutputStream fos = openFileOutput(TEXT_FILE, Context.MODE_PRIVATE);
+                    fos.write(text.getBytes());
+                    fos.close();
+
+                    Log.d(DEBUG_TAG, "Saved file to internal storage.");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d(DEBUG_TAG, "Unable to save file to internal storage.");
+                }
+
             }
         });
     }
