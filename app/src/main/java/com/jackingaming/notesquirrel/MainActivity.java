80f25ca7e2b22f12e3680cc7e828d3b4f6dfbc6f
jamesgov <blindmice1o3@gmail.com>
2020-02-16 12:32:01 -0800

    feat: Add load functionality
    During app start up (the onCreate() method call), the content of a file (saved/written during a prior "save" button click) is read and subsequently appended to the EditText (a View component) object.
    
    FUTURE-EXERCISE: Try creating a  new Button component, name it "load" and attach an id to it, lay it out to the right of the "save" button, set an OnClickListener, and move the loadSavedFile() call out of the onCreate() and into the implementation of OnClickListener's onClick() method.

--- app/src/main/java/com/jackingaming/notesquirrel/MainActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/MainActivity.java
@@ -9,8 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addSaveButtonListener();
+
+        loadSavedFile();
+    }
+
+    private void loadSavedFile() {
+        try {
+            FileInputStream fis = (FileInputStream) openFileInput(TEXT_FILE);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    new DataInputStream(fis)));
+
+            EditText editText = (EditText) findViewById(R.id.text);
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                editText.append(line);
+                editText.append("\n");
+            }
+
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(DEBUG_TAG, "Unable to read file to internal storage.");
+        }
     }
 
     private void addSaveButtonListener() {
@@ -32,8 +61,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Log.d(DEBUG_TAG, "Save button clicked: " + text);
-                    //was used to verify the button was actually "doing stuff"
-                    //when we clicked it (before we supplied implementation).
+                //was used to verify the button was actually "doing stuff"
+                //when we clicked it (before we supplied implementation).
 
                 EditText editText = (EditText) findViewById(R.id.text);
 
