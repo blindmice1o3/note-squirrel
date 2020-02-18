1fcd7880359bfa4c95ddc73e1a7765972c26b61e
jamesgov <blindmice1o3@gmail.com>
2020-02-18 09:17:02 -0800

    feat: Add Dialog message to app's starting Activity
    Needed a builder, set options (OK/Cancel buttons, title, and the actual message/instruction prompt to the user) on the builder, have the builder "create" the Dialog instance, and "show" the Dialog instance to the device's screen.
    
    Could have added additional functionality to the user clicking the "OK" (and/or "Cancel") button, but intentionally left it empty/blank because we don't need additional functionality in this case).

--- app/src/main/java/com/jackingaming/notesquirrel/ImageActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/ImageActivity.java
@@ -2,6 +2,8 @@ package com.jackingaming.notesquirrel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,6 +20,30 @@ public class ImageActivity extends AppCompatActivity {
 
         //MUST come after setContentView(int).
         addTouchListener();
+        //displays a Dialog message to the device's screen.
+        showPrompt();
+    }
+
+    private void showPrompt() {
+        //Builder is probably an inner-class of the AlertDialog class.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //setting up buttons on the builder (to customize the dialog that it will build).
+        //setNegativeButton(CharSequence, OnClickListener) would be like the "cancel" button.
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //intentionally left blank.
+            }
+        });
+
+        builder.setTitle("Create Your Passpoint Sequence");
+        builder.setMessage("Touch four points on the image to set the passpoint sequence. You must click the same points in future to gain access to your notes.");
+
+        //use the Builder to create the dialog (set various options on the builder and then call create()).
+        //this will return an AlertDialog object (it can have an "Ok" and/or "Cancel" button[s]).
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     private void addTouchListener() {
@@ -35,11 +61,10 @@ public class ImageActivity extends AppCompatActivity {
                 Log.d(MainActivity.DEBUG_TAG, message);
 
                 //display message to device's screen via a Toast pop-up.
-                /* (discovered that Toast is slow, and it continues to show back-logged Toasts even after app shut down.)
+                //(discovered that Toast is slow, and it continues to show back-logged Toasts even after app shut down.)
+                //TODO: remove this Toast-developer_helper.
                 Toast toastCantSave = Toast.makeText(ImageActivity.this, message, Toast.LENGTH_SHORT);
                 toastCantSave.show();
-                */
-
 
                 return false;
             }
