7891aa20a8f215e6728c1f4e0850707ccf83db31
jamesgov <blindmice1o3@gmail.com>
2020-02-17 21:40:30 -0800

    feat: Add OnTouchListener to ImageView component
    The x and y coordinate values of touch (aka click) events are obtained. The coordinates are displayed to the Logcat monitor using "Log.d(String, String)".
    
    Experimented with Toast and found that it will show ALL OF THE queued-up Toast-messages EVEN AFTER THE APP IS SHUT DOWN.

--- app/src/main/java/com/jackingaming/notesquirrel/ImageActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/ImageActivity.java
@@ -3,6 +3,11 @@ package com.jackingaming.notesquirrel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class ImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        //MUST come after setContentView(int).
+        addTouchListener();
     }
+
+    private void addTouchListener() {
+        ImageView image = (ImageView) findViewById(R.id.touch_image);
+
+        image.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getX();
+                float y = event.getY();
+
+                String message = String.format("Coordinates: (%.2f, %.2f)", x, y);
+
+                //display message to Logcat monitor.
+                Log.d(MainActivity.DEBUG_TAG, message);
+
+                //display message to device's screen via a Toast pop-up.
+                /* (discovered that Toast is slow, and it continues to show back-logged Toasts even after app shut down.)
+                Toast toastCantSave = Toast.makeText(ImageActivity.this, message, Toast.LENGTH_SHORT);
+                toastCantSave.show();
+                */
+
+
+                return false;
+            }
+        });
+    }
+
 }
