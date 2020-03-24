79f7250b254742221d28929e755ead46f8b5247c
jamesgov <blindmice1o3@gmail.com>
2020-03-24 15:54:34 -0700

    refactor: Dynamically populate ListViewerActivity's ListView
    The ListView View-component is no longer "hard-coding" the list items within its associated xml layout file (where the ListView element had its "android:entries" attribute set directly). The list items are now specified within ListViewerActivity.setListListener() and are adapted from an array of String to list items.

--- app/src/main/java/com/jackingaming/notesquirrel/ListViewerActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/ListViewerActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,7 +20,23 @@ public class ListViewerActivity extends AppCompatActivity {
     }
 
     private void setListListener() {
+        //dynamically populating the ListView object
+        String[] values = getResources().getStringArray(R.array.list_book_title);
+
+        //demonstrating that it's dynamically loading the list item
+        //values[0] = "Virtual Light";
+
+        //transform a bunch of String objects (from the array) into list items
+            //the "R" is not the one we've used before (relative to this current project), it's
+            //actually "android.R" which is a predefined constant from android (xml layout files).
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+
         ListView list = (ListView) findViewById(R.id.list);
+
+        //no longer "hard-coded" into the layout xml file; now the list items are loaded dynamically
+        /////////////////////////
+        list.setAdapter(adapter);
+        /////////////////////////
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
