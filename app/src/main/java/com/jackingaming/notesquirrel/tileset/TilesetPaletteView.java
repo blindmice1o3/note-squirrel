8ad91e276791749f52928b48a7e08c0d8ce64ec3
jamesgov <blindmice1o3@gmail.com>
2020-03-30 13:49:11 -0700

    note: Setup aspect-ratio calculations for canvas size and the Bitmap tileset's size

--- app/src/main/java/com/jackingaming/notesquirrel/tileset/TilesetPaletteView.java
+++ app/src/main/java/com/jackingaming/notesquirrel/tileset/TilesetPaletteView.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -20,8 +21,17 @@ import com.jackingaming.notesquirrel.R;
 
 public class TilesetPaletteView extends ImageView {
 
-    private int tileSize;
+    private int widthCanvas;
+    private int heightCanvas;
+
     private Bitmap tileset;
+
+    private Rect canvasScreenRect;
+    private Rect tilesetRect;
+
+    private int numberOfTilesAcross;
+    private int tileSize;
+    private int numberOfTilesDown;
 
     private int xSelected;
     private int ySelected;
@@ -30,10 +40,22 @@ public class TilesetPaletteView extends ImageView {
         super(context, attrs);
         Log.d(MainActivity.DEBUG_TAG, "TilesetPaletteView(Context) PRE-BitmapFactory.decodeResource(Resources, int).");
 
-        tileSize = 40;
+        numberOfTilesAcross = 6;
+
         tileset = BitmapFactory.decodeResource(context.getResources(), R.drawable.pc_computer_yoko_tileset);
         Log.d(MainActivity.DEBUG_TAG, "TilesetPaletteView(Context) POST-BitmapFactory.decodeResource(Resources, int).");
-        setImageBitmap(tileset);
+        //setImageBitmap(tileset);
+        tilesetRect = new Rect(0, 0, tileset.getWidth(), tileset.getHeight());
+
+        Log.d(MainActivity.DEBUG_TAG, "tileSize = tilesetRect.right / numberOfTilesAcross: " + tilesetRect.right + " / " + numberOfTilesAcross);
+        tileSize = tilesetRect.right / numberOfTilesAcross;
+        Log.d(MainActivity.DEBUG_TAG, "tileSize: " + tileSize);
+
+        Log.d(MainActivity.DEBUG_TAG, "numberOfTilesDown = tilesetRect.bottom / tileSize: " + tilesetRect.bottom + " / " + tileSize);
+        numberOfTilesDown = tilesetRect.bottom / tileSize;
+        Log.d(MainActivity.DEBUG_TAG, "numberOfTilesDown: " + numberOfTilesDown);
+
+        setBackgroundResource(R.drawable.pc_computer_yoko_tileset);
 
         setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -57,13 +79,31 @@ public class TilesetPaletteView extends ImageView {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.d(MainActivity.DEBUG_TAG, "###################################################################");
+        Log.d(MainActivity.DEBUG_TAG, "#######TilesetPaletteView.onSizeChanged(int, int, int, int)#######");
+
+        widthCanvas = w;
+        heightCanvas = h;
+
+        canvasScreenRect = new Rect(0, 0, w, h);
+
+        Log.d(MainActivity.DEBUG_TAG, "(widthCanvas, heightCanvas): " + widthCanvas + ", " + heightCanvas);
+        Log.d(MainActivity.DEBUG_TAG, "###################################################################");
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Log.d(MainActivity.DEBUG_TAG, "@@@@@@@@@@@@@@@@@@@@@@@@@@TilesetPaletteView.onDraw(Canvas).@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        Log.d(MainActivity.DEBUG_TAG, "(canvas.getWidth(), canvas.getHeight()): " + canvas.getWidth() + ", " + canvas.getHeight());
 
         Paint paint = new Paint();
         paint.setColor(Color.YELLOW);
         canvas.drawRect(xSelected, ySelected, (xSelected + tileSize), (ySelected + tileSize), paint);
+
+        Log.d(MainActivity.DEBUG_TAG, "@@@@@@@@@@@@@@@@@@@@@@@@@@TilesetPaletteView.onDraw(Canvas).@@@@@@@@@@@@@@@@@@@@@@@@@@");
     }
 
     public void setSelectedTileCoordinates(int xSelected, int ySelected) {
