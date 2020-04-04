f0b1c5ca2bdeb2e195c5c7c460da5610e89f2655
jamesgov <blindmice1o3@gmail.com>
2020-04-04 15:38:41 -0700

    feat: Add "PC MS Office Clippit" to Assets
    FragmentParentActivity's button now iterate through Assets.entities to verify that sprites were cropped from the spritesheet properly.

--- app/src/main/java/com/jackingaming/notesquirrel/learn_fragments/Assets.java
+++ app/src/main/java/com/jackingaming/notesquirrel/learn_fragments/Assets.java
@@ -11,12 +11,53 @@ import com.jackingaming.notesquirrel.R;
 public class Assets {
 
     public static Bitmap[][] items;
+    public static Bitmap[][] entities;
 
     public static void init(Resources resources) {
         initItems(resources);
         //initEntities(resources);
         //initTiles(resources);
         //etc
+
+        initEntities(resources);
+    }
+    private static void initEntities(Resources resources) {
+        //LOAD SPRITESHEET
+        Bitmap imageSource = BitmapFactory.decodeResource(resources, R.drawable.pc_ms_office_clippit);
+
+        //CHECK IF IMAGE RESOURCE DID not LOAD PROPERLY
+        if (imageSource == null) {
+            Log.d(MainActivity.DEBUG_TAG, "Assets.initEntities(Resources): imageSource == null");
+
+            Log.d(MainActivity.DEBUG_TAG, "BAILING EARLY!!!!!");
+            return;
+        }
+
+        //IF WE'VE MADE IT THIS FAR, THE IMAGE RESOURCE LOADED AS INTENDED
+        Log.d(MainActivity.DEBUG_TAG, "Assets.initEntities(Resources): imageSource != null");
+        Log.d(MainActivity.DEBUG_TAG, "Assets.initEntities(Resources): imageSource.getWidth(), imageSource.getHeight() : " + imageSource.getWidth() + ", " + imageSource.getHeight());
+        //UNPACKING THE SPRITESHEET (logic is specific to each spritesheet's layout)
+        int column = 22;
+        int row = 41;
+
+        entities = new Bitmap[row][column];
+
+        int margin = 0;
+        int tileWidth = 124;
+        int tileHeight = 93;
+
+        int xCurrent = margin;
+        int yCurrent = margin;
+
+        for (int y = 0; y < row; y++) {
+            for (int x = 0; x < column; x++) {
+                entities[y][x] = Bitmap.createBitmap(imageSource, xCurrent, yCurrent, tileWidth, tileHeight);
+                xCurrent += (tileWidth + margin);
+            }
+            xCurrent = margin;
+            yCurrent += (tileHeight + margin);
+        }
+        Log.d(MainActivity.DEBUG_TAG, "Assets.initEntities(Resources): FINISHED!!!");
     }
 
     private static void initItems(Resources resources){
@@ -25,14 +66,14 @@ public class Assets {
 
         //CHECK IF IMAGE RESOURCE DID not LOAD PROPERLY
         if (imageSource == null) {
-            Log.d(MainActivity.DEBUG_TAG, "Assets.init(Resources): imageSource == null");
+            Log.d(MainActivity.DEBUG_TAG, "Assets.initItems(Resources): imageSource == null");
 
             Log.d(MainActivity.DEBUG_TAG, "BAILING EARLY!!!!!");
             return;
         }
 
         //IF WE'VE MADE IT THIS FAR, THE IMAGE RESOURCE LOADED AS INTENDED
-        Log.d(MainActivity.DEBUG_TAG, "Assets.init(Resources): imageSource != null");
+        Log.d(MainActivity.DEBUG_TAG, "Assets.initItems(Resources): imageSource != null");
 
         //UNPACKING THE SPRITESHEET (logic is specific to each spritesheet's layout)
         int column = 9;
@@ -55,6 +96,7 @@ public class Assets {
             xCurrent = margin;
             yCurrent += (tileHeight + margin);
         }
+        Log.d(MainActivity.DEBUG_TAG, "Assets.initItems(Resources): FINISHED!!!");
     }
 
 }
--- app/src/main/java/com/jackingaming/notesquirrel/learn_fragments/FragmentParentActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/learn_fragments/FragmentParentActivity.java
@@ -12,6 +12,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.jackingaming.notesquirrel.MainActivity;
@@ -35,11 +36,19 @@ public class FragmentParentActivity extends AppCompatActivity {
         ////////////////////////////
         Assets.init(getResources());
         ////////////////////////////
-        spriteSheetItems = Assets.items;
+        //spriteSheetItems = Assets.items;
+        spriteSheetItems = Assets.entities;
 
-        imageSource = BitmapFactory.decodeResource(getResources(), R.drawable.gbc_hm2_spritesheet_items);
+
+
+        //imageSource = BitmapFactory.decodeResource(getResources(), R.drawable.gbc_hm2_spritesheet_items);
+        Bitmap tooBigClippit = BitmapFactory.decodeResource(getResources(), R.drawable.pc_ms_office_clippit);
+        Bitmap justRightClippit = Bitmap.createScaledBitmap(tooBigClippit, 500, 500, false);
+        imageSource = justRightClippit;
+        //imageSource = BitmapFactory.decodeResource(getResources(), R.drawable.pc_ms_office_clippit);
         imageView = (ImageView) findViewById(R.id.imageview_fragment);
-        imageView.setImageBitmap(imageSource);
+        //imageView.setImageBitmap(imageSource);
+        imageView.setImageBitmap(justRightClippit);
         Log.d(MainActivity.DEBUG_TAG, "imageSource.getWidth(): " + imageSource.getWidth());
         Log.d(MainActivity.DEBUG_TAG, "imageSource.getHeight(): " + imageSource.getHeight());
         /*
@@ -75,8 +84,10 @@ public class FragmentParentActivity extends AppCompatActivity {
                 //imageView.setAdjustViewBounds(true);
                 Log.d(MainActivity.DEBUG_TAG, "button's OnClickListener.onClick(View) BEFORE calling spriteSheet's width and height.");
                 //How to set by pixel:
-                imageView.getLayoutParams().width = (spriteSheetItems[indexButtonY][indexButtonX].getWidth() * 15);
-                imageView.getLayoutParams().height = (spriteSheetItems[indexButtonY][indexButtonX].getHeight() * 15);
+                //imageView.getLayoutParams().width = (spriteSheetItems[indexButtonY][indexButtonX].getWidth() * 15);
+                imageView.getLayoutParams().width = (spriteSheetItems[indexButtonY][indexButtonX].getWidth() * 8);
+                //imageView.getLayoutParams().height = (spriteSheetItems[indexButtonY][indexButtonX].getHeight() * 15);
+                imageView.getLayoutParams().height = (spriteSheetItems[indexButtonY][indexButtonX].getHeight() * 8);
                 //imageView.setLayoutParams( new ViewGroup.LayoutParams(16*15, 16*15) );
                 //////////////////////////
                 imageView.requestLayout();
@@ -95,13 +106,24 @@ public class FragmentParentActivity extends AppCompatActivity {
                 imageView.setImageBitmap(newImage);
 
                 indexButtonX++;
-                if ((indexButtonX%9) == 0) {
+                if (indexButtonX == spriteSheetItems[indexButtonY].length) {
                     indexButtonX = 0;
                     indexButtonY++;
+
+                    if (indexButtonY == spriteSheetItems.length) {
+                        indexButtonY = 0;
+                    }
                 }
 
-
-
+                /*
+                int[][] test = {
+                        {1, 2},
+                        {3, 4},
+                        {5, 6},
+                        {7, 8}
+                };
+                Log.d(MainActivity.DEBUG_TAG, "test.length, test[0].length: " + test.length + ", " + test[0].length);
+                */
 
                 Log.d(MainActivity.DEBUG_TAG,
                         "imageView.getWidth(), imageView.getHeight(): " +
