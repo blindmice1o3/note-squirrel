2bb650f52c3b5de35c2c9bece28ad253bdabe6ff
jamesgov <blindmice1o3@gmail.com>
2020-04-06 17:34:09 -0700

    note: Experiment with DisplayMetrics

--- app/src/main/java/com/jackingaming/notesquirrel/learn_fragments2/ModelFragment.java
+++ app/src/main/java/com/jackingaming/notesquirrel/learn_fragments2/ModelFragment.java
@@ -2,6 +2,7 @@ package com.jackingaming.notesquirrel.learn_fragments2;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.jackingaming.notesquirrel.MainActivity;
 import com.jackingaming.notesquirrel.R;
 
 public class ModelFragment extends Fragment {
@@ -36,7 +38,16 @@ public class ModelFragment extends Fragment {
         imageView.setImageDrawable(icon);
         imageView.getLayoutParams().height = (int) (icon.getIntrinsicHeight() * 0.5);
         imageView.setAdjustViewBounds(true);
-
+        ///////////////////////////////////////////////////////////////////////
+        float scaledDensity = getResources().getDisplayMetrics().scaledDensity;
+        int widthPixels = getResources().getDisplayMetrics().widthPixels;
+        int heightPixels = getResources().getDisplayMetrics().heightPixels;
+        Log.d(MainActivity.DEBUG_TAG, "scaledDensity: " + scaledDensity);
+        Log.d(MainActivity.DEBUG_TAG, "widthPixels: " + widthPixels);
+        Log.d(MainActivity.DEBUG_TAG, "heightPixels: " + heightPixels);
+        Log.d(MainActivity.DEBUG_TAG, "icon.getInstrinsicHeight(): " + icon.getIntrinsicHeight());
+        Log.d(MainActivity.DEBUG_TAG, "icon.getIntrinsicHeight() * 0.5: " + (icon.getIntrinsicHeight() * 0.5));
+        ///////////////////////////////////////////////////////////////////////
         textView.setText("They told Jack Burton to go to hell... and that's exactly where he's going!");
 
         return view;
