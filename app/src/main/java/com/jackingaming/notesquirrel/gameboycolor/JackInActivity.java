7bab65b3890a6562d38bb76f8d9407fcfe006905
jamesgov <blindmice1o3@gmail.com>
2020-04-15 10:57:00 -0700

    feat: Add "swap game" button to switch GameCartridge
    In GameView.switchGame(boolean), the presently running thread is shutdown, a new instance of GameCartridge is instantiated, and a new thread will run.
    
    The previous "run" of PoohFarmerCartridge is not saved and therefore the user is starting over from the beginning of the game. This is also true for PongCartridge (but isn't as significant because "runs" of this game is much shorter).
    
    The screen's user input layout doesn't change when in PongCartridge (there's still a directional pad and the "swap game" button in the bottom half of the screen (this game is no longer full screen).

--- app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/JackInActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/JackInActivity.java
@@ -3,15 +3,31 @@ package com.jackingaming.notesquirrel.gameboycolor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jackingaming.notesquirrel.R;
 
 public class JackInActivity extends AppCompatActivity {
 
+    private boolean isPoohFarmer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jack_in);
+
+        final GameView gameView = (GameView) findViewById(R.id.gameView);
+        Button swapGameButton = (Button) findViewById(R.id.swap_game);
+        isPoohFarmer = true;
+
+        swapGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPoohFarmer = !isPoohFarmer;
+                gameView.switchGame(isPoohFarmer);
+            }
+        });
 
         /*
         GameView gameView = (GameView) findViewById(R.id.game);
