f388667152730f11f3640c06ed48007330466caa
jamesgov <blindmice1o3@gmail.com>
2020-04-22 08:12:01 -0700

    tinker: Add second ImageView to TinkerMotionEventActivity
    Experimenting with how to detect MotionEvent.ACTION_MOVE entering another View's bounds when the MotionEvent originates (MotionEvent.ACTION_DOWN) from a different View.

--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/tinkermotionevent/TinkerMotionEventActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/tinkermotionevent/TinkerMotionEventActivity.java
@@ -2,6 +2,7 @@ package com.jackingaming.notesquirrel.sandbox.tinkermotionevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,24 +19,68 @@ public class TinkerMotionEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tinker_motion_event);
 
-        ImageView imageView = (ImageView) findViewById(R.id.tinker_motion_event);
-        imageView.setOnTouchListener(new View.OnTouchListener() {
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1MotionEvent);
+        imageView1.setOnTouchListener(new View.OnTouchListener() {
+
+            private Rect rectBoundsOfView1;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity.imageView.OnTouchListener...");
+                Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity.imageView1.OnTouchListener... 111");
+
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_DOWN");
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_DOWN 111");
+
+                    rectBoundsOfView1 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
                 } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_MOVE");
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_MOVE 111");
+
+                    if( !rectBoundsOfView1.contains(
+                            v.getLeft() + (int)event.getX(),
+                            v.getTop() + (int)event.getY()) ) {
+                        Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_MOVE 111 NOT inside rectBoundsOfView1");
+                    }
                 } else if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
-                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_OUTSIDE");
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_OUTSIDE 111");
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_UP");
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_UP 111");
                 }
 
                 return true;
             }
         });
+
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView2MotionEvent);
+        imageView2.setOnTouchListener(new View.OnTouchListener() {
+
+            private Rect rectBoundsOfView2;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity.imageView2.OnTouchListener... 222");
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_DOWN 222");
+
+                    rectBoundsOfView2 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_MOVE 222");
+
+                    if( !rectBoundsOfView2.contains(
+                            v.getLeft() + (int)event.getX(),
+                            v.getTop() + (int)event.getY()) ) {
+                        Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_MOVE 222 NOT inside rectBoundsOfView2");
+                    }
+                } else if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_OUTSIDE 222");
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Log.d(MainActivity.DEBUG_TAG, "TinkerMotionEventActivity MotionEvent.ACTION_UP 222");
+                }
+
+                return true;
+            }
+        });
+
     }
 
 }
