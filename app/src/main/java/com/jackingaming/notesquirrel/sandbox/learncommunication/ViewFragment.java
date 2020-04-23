60bfaf1806fc2587cce7149b656c53df929763f4
jamesgov <blindmice1o3@gmail.com>
2020-04-23 10:51:18 -0700

    feat: Add vertical scrolling movement for CommunicationActivity's ViewFragment's TextView
    The user can now scroll through the downloaded HTML.

--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/learncommunication/ViewFragment.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/learncommunication/ViewFragment.java
@@ -1,10 +1,12 @@
 package com.jackingaming.notesquirrel.sandbox.learncommunication;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,7 +29,8 @@ public class ViewFragment extends Fragment {
         Log.d(MainActivity.DEBUG_TAG, "ViewFragment.onCreateView(LayoutInflater, ViewGroup, Bundle)");
         View view = inflater.inflate(R.layout.fragment_view, container, false);
 
-
+        TextView textViewCommunication = (TextView) view.findViewById(R.id.textView_communication);
+        textViewCommunication.setMovementMethod(new ScrollingMovementMethod());
 
         return view;
     }
