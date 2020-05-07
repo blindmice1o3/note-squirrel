3178b2a2eabac96932877a07f136e244770e08f3
jamesgov <blindmice1o3@gmail.com>
2020-05-06 22:00:07 -0700

    tinker: Load, split, count "dvd_library_collection_unsorted_csv.txt"
    There are 525 comma-seperated-values.

--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/gridviewdvd/GridViewDvdActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/gridviewdvd/GridViewDvdActivity.java
@@ -3,8 +3,15 @@ package com.jackingaming.notesquirrel.sandbox.gridviewdvd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.jackingaming.notesquirrel.MainActivity;
 import com.jackingaming.notesquirrel.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class GridViewDvdActivity extends AppCompatActivity {
 
@@ -12,6 +19,29 @@ public class GridViewDvdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_view_dvd);
+
+        BufferedReader bufferedReader = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+
+        InputStream inputStream = getResources().openRawResource(R.raw.dvd_library_collection_unsorted_csv);
+
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] dvds = stringBuilder.toString().split(",");
+        Log.d(MainActivity.DEBUG_TAG, "number of dvds: " + dvds.length);
+        int counter = 0;
+        for (String dvd : dvds) {
+            counter++;
+            Log.d(MainActivity.DEBUG_TAG, String.format("%3d: %s", counter, dvd));
+        }
     }
 
 }
