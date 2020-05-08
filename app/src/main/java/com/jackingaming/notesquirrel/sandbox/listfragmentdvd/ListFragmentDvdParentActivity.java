45beb6f16769e8cd3207b371b2c4c96a5a166f3e
jamesgov <blindmice1o3@gmail.com>
2020-05-07 18:10:40 -0700

    Add action items (listview and gridview) to the app bar
    Created my own icon images and attached them to menu items (with attribute "showAsAction") in ListFragmentDvdParent (the dvd library with IMAGE resource [the learning-grid-view version of the dvd library is currently just a bunch of String for titles without images]).
    
    The action items representing "ListView" and "GridView" are presently outputting a toast-message, rather than switching layout.

--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/listfragmentdvd/ListFragmentDvdParentActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/listfragmentdvd/ListFragmentDvdParentActivity.java
@@ -1,10 +1,14 @@
 package com.jackingaming.notesquirrel.sandbox.listfragmentdvd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.jackingaming.notesquirrel.MainActivity;
 import com.jackingaming.notesquirrel.R;
@@ -48,4 +52,26 @@ public class ListFragmentDvdParentActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_list_fragment_dvd_parent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_listview:
+                //TODO: implement menu_listview
+                Toast.makeText(this, "ListView", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_gridview:
+                //TODO: implement menu_gridview
+                Toast.makeText(this, "GridView", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                Toast.makeText(this, "ListFragmentDvdParentActivity.onOptionsItemSelected(MenuItem) switch's default", Toast.LENGTH_SHORT).show();
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
