66e22dffdf91bfc9b1bc9661d0b7329544bacf33
jamesgov <blindmice1o3@gmail.com>
2020-05-12 10:06:58 -0700

    tinker: Able to add and remove fragment programmatically
    Had trouble finding the fragment by id.

--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/learnfragment/LearnFragmentParentActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/learnfragment/LearnFragmentParentActivity.java
@@ -1,6 +1,8 @@
 package com.jackingaming.notesquirrel.sandbox.learnfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,14 +21,57 @@ public class LearnFragmentParentActivity extends AppCompatActivity
     public void onFragmentInteraction(Uri uri) {
         Toast.makeText(this, "LearnFragmentParentActivity's fragment's interface... FirstTestFragment.OnFragmentInteractionListener.onFragmentInteraction(Uri)", Toast.LENGTH_SHORT).show();
     }
-
+    
     public void onLeftButtonClick(View view) {
         Log.d(MainActivity.DEBUG_TAG, "LearnFragmentParentActivity.onLeftButtonClick(View)");
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FirstTestFragment framelayoutFirstTestFragment = (FirstTestFragment) fragmentManager.findFragmentById(R.id.framelayout_first_test_fragment);
+        FirstTestFragment textviewFirstTestFragment = (FirstTestFragment) fragmentManager.findFragmentById(R.id.textview_first_test_fragment);
+        FirstTestFragment framelayouttopFirstTestFragment = (FirstTestFragment) fragmentManager.findFragmentById(R.id.framelayout_top);
+        if (framelayoutFirstTestFragment == null) {
+            Log.d(MainActivity.DEBUG_TAG, "framelayoutFirstTestFragment is null");
+        }
+        if (textviewFirstTestFragment == null) {
+            Log.d(MainActivity.DEBUG_TAG, "textviewFirstTestFragment is null");
+        }
+        if (framelayouttopFirstTestFragment == null) {
+            Log.d(MainActivity.DEBUG_TAG, "framelayouttopFirstTestFragment is null");
+        } else {
+            Log.d(MainActivity.DEBUG_TAG, "framelayouttopFirstTestFragment is NOT null");
+        }
+
+
+        if (framelayouttopFirstTestFragment != null ) {
+            // && framelayouttopFirstTestFragment.isInLayout()
+            Log.d(MainActivity.DEBUG_TAG, "LearnFragmentParentActivity.onLeftButtonClick(View)... remove(Fragment)");
+
+            //fragmentManager.popBackStack();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.remove(framelayouttopFirstTestFragment);
+
+            transaction.commit();
+        } else {
+            Log.d(MainActivity.DEBUG_TAG, "LearnFragmentParentActivity.onLeftButtonClick(View)... NOT remove(Fragment)");
+        }
     }
 
     public void onRightButtonClick(View view) {
         Log.d(MainActivity.DEBUG_TAG, "LearnFragmentParentActivity.onRightButtonClick(View)");
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        FirstTestFragment fragment = FirstTestFragment.newInstance("one", "two");
+        transaction.add(R.id.framelayout_top, fragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+
+        //////////////////////////////////////////////////////////////////////////////////
+        Log.d(MainActivity.DEBUG_TAG, "FirstTestFragment's id: " + fragment.getId());
+        //////////////////////////////////////////////////////////////////////////////////
     }
 
     @Override
