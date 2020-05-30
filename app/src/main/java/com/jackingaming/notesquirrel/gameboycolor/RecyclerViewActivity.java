b2e2923ca6590b21402c19da2965146f5d845a95
jamesgov <blindmice1o3@gmail.com>
2020-05-29 19:45:39 -0700

    feat: Add "switch mode" button to RecyclerViewActivity
    The button alternate the LayoutManager between GridLayoutManager and LinearLayoutManager.

--- app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/RecyclerViewActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/RecyclerViewActivity.java
@@ -2,14 +2,17 @@ package com.jackingaming.notesquirrel.gameboycolor;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
 
+import com.jackingaming.notesquirrel.MainActivity;
 import com.jackingaming.notesquirrel.R;
 import com.jackingaming.notesquirrel.gameboycolor.gamecartridges.GameCartridge;
 import com.jackingaming.notesquirrel.gameboycolor.gamecartridges.pocketcritters.PocketCrittersCartridge;
@@ -17,12 +20,18 @@ import com.jackingaming.notesquirrel.gameboycolor.gamecartridges.pocketcritters.
 public class RecyclerViewActivity extends AppCompatActivity
         implements AdapterRecyclerView.ItemClickListener {
 
+    public enum Mode { GRID, LINEAR; }
+
+    private Mode mode = Mode.GRID;
+
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_tinkering);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_tinkering);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -47,4 +56,30 @@ public class RecyclerViewActivity extends AppCompatActivity
     public void onItemClick(View view, int position) {
         Toast.makeText(this, "position: " + position, Toast.LENGTH_SHORT).show();
     }
+
+    public void onSwitchModeButtonClick(View view) {
+        Log.d(MainActivity.DEBUG_TAG, "RecyclerViewActivity.onSwitchModeButtonClick(View)");
+
+        // Alternate current mode.
+        mode = (mode == Mode.GRID) ? (Mode.LINEAR) : (Mode.GRID);
+
+        RecyclerView.LayoutManager layoutManager = null;
+        switch (mode) {
+            case GRID:
+                // use a grid layout manager
+                int numberOfColumns = 4;
+                layoutManager = new GridLayoutManager(this, numberOfColumns);
+                break;
+            case LINEAR:
+                // use a linear layout manager
+                layoutManager = new LinearLayoutManager(this);
+                break;
+            default:
+                Log.d(MainActivity.DEBUG_TAG, "RecyclerViewActivity.onSwitchModeButtonClick(View) switch (mode)'s default block.");
+                break;
+        }
+
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
 }
