863e1e5d924eaff8dcf1d50f5f973b52624c6733
jamesgov <blindmice1o3@gmail.com>
2020-06-07 19:09:04 -0700

    refactor: Add line-wrapping for TextboxState
    Have NOT implemented NEXT page if the text fills up more than the maximum vertical space of the textbox.

--- app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/gamecartridges/pocketcritters/states/TextboxState.java
+++ app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/gamecartridges/pocketcritters/states/TextboxState.java
@@ -127,27 +127,58 @@ public class TextboxState
             //Clear the canvas by painting the background white.
             canvas.drawColor(Color.WHITE);
 
-            //RE-DRAW state.GAME as background (otherwise white background because we'd cleared earlier)
+            //@@@RE-DRAW state.GAME as background@@@
+            // (otherwise white background because we'd cleared earlier)
             sceneManager.getCurrentScene().render(canvas);
 
-
-
-            //DRAW BACKGROUND
+            //@@@DRAW BACKGROUND@@@
             int xStartBackground = 0;
             int yStartBackground = (int)((2/3f) * heightViewport);
             Rect background = new Rect(xStartBackground, yStartBackground, widthViewport, heightViewport);
             canvas.drawRect(background, paintBackground);
 
-            //DRAW TEXT
+            //TODO:
+            // (1) split full text into line-size-chunks
+            // (2) determine how many lines will fit
+
+            //@@@DRAW TEXT@@@
+            // Area to work with.
             int padding = 10;
+            int heightMax = heightViewport - yStartBackground - padding - padding;
+            int widthMax = widthViewport - xStartBackground - padding - padding;
+
             Paint.FontMetrics fm = paintText.getFontMetrics();
-            int lineHeight = (int) (fm.bottom - fm.top); // maybe "+ fm.leading"
+            int lineHeight = (int) (fm.bottom - fm.top + fm.leading);
 
             int xStartText = xStartBackground + padding;
             int yStartText = yStartBackground + lineHeight; //normally starts top-left
-            String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            canvas.drawText(text, xStartText, yStartText, paintText);
 
+
+            // Complete text to display.
+            //String textFull = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            String textFull = "The cat in the hat will never give a fish what it desires most, the key to the city of moonlight. This is true for fall, winter, and spring... but NOT summer. In the summer, the fashionable feline's generousity cresses before breaking into a surge of outward actions which benefit the entire animal community, far more than just that of fishes who desire the key to the city of moonlight. Unfortunately, summer passes quicker than most fish would like.";
+            String[] words = textFull.split(" ");
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < words.length; i++) {
+                if ((paintText.measureText(sb.toString()) + paintText.measureText(words[i] + " "))
+                        < widthMax) {
+                    sb.append(words[i]);
+                    sb.append(" ");
+                } else {
+                    //NEXT WORD WOULDN'T FIT ON LINE... DO NOT INCREMENT i!!!
+                    i--;
+
+                    //DRAW THE CURRENT LINE and RESTART StringBuilder!!!
+                    /////////////////////////////////////////////////////////////
+                    canvas.drawText(sb.toString(), xStartText, yStartText, paintText);
+                    /////////////////////////////////////////////////////////////
+                    sb = new StringBuilder();
+                    yStartText += lineHeight;
+
+                    //TODO: if yStartText reaches heightMax... must start a new page.
+                }
+            }
 
 
             //unlock it and post our updated drawing to it.
