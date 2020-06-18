ac47f22a264984237dc3a5751c4b05b557270101
jamesgov <blindmice1o3@gmail.com>
2020-06-18 10:15:47 -0700

    feat: Add tile collision detection to hothouse scene
    The hothouse scene's TileMap's datasource now has properly laid out solid tiles.

---
+++ app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/gamecartridges/derived/pocketcritters/computer/QuadrantActivity.java
@@ -0,0 +1,14 @@
+package com.jackingaming.notesquirrel.gameboycolor.gamecartridges.derived.pocketcritters.computer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuadrantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadrant);
+    }
+}
