856218b8113f047f515b9bf5e7bd17bcf3a82724
jamesgov <blindmice1o3@gmail.com>
2020-10-02 16:56:58 -0700

    tinker: Update CountZeroActivity to use Switch widgets
    Previous implementation used a ListView with an adapter that defined how each list-item is displayed (in this case, selecting an image for an ImageView based on each entry's isActive field).
    
    Current implementation uses a ScrollView with one child (a LinearLayout loaded with Switch Views [widgets]). It's also using October dates.
    
    SUMMARY: Experimenting with a new layout component.

--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/countzero/CountZeroActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/countzero/CountZeroActivity.java
@@ -1,87 +1,109 @@
 package com.jackingaming.notesquirrel.sandbox.countzero;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import com.jackingaming.notesquirrel.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CountZeroActivity extends AppCompatActivity {
 
-    private static final int START_OF_MONTH = 11;
-    private static final int END_OF_MONTH = 30;
-
-    private List<Entry> dates;
+    private static final int START_OF_MONTH = 1;
+    private static final int END_OF_MONTH = 31;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_count_zero);
+        setContentView(R.layout.activity_count_zero_scrollview_with_switches);
 
-        initializeDates();
-        inflateListViewUsingDates();
-
-        loadIsActiveColumn();
+        initializeSwitchViews();
     }
 
-    private void initializeDates() {
-        dates = new ArrayList<Entry>();
+    private void initializeSwitchViews() {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearlayout_for_switches);
+        LinearLayout.LayoutParams layoutParamsOfSwitchViews = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        //SEPTEMBER
-        for (int i = START_OF_MONTH; i <= END_OF_MONTH; i++) {
-            String date = String.format("2020_09_%02d", i);
-            Entry entry = new Entry(date, false);
+        for (int day = START_OF_MONTH; day <= END_OF_MONTH; day++) {
+            String textOfLabel = String.format("2020_10_%02d", day);
 
-            dates.add(entry);
+            Switch switchView = new Switch(this);
+            switchView.setChecked(false);
+            switchView.setLayoutParams(layoutParamsOfSwitchViews);
+            switchView.setPadding(20, 10, 0, 10);
+            switchView.setGravity(Gravity.START);
+
+            int textColor = 0;
+            int backgroundColor = 0;
+            if (day % 2 == 0) {
+                textColor = Color.WHITE;
+                backgroundColor = Color.CYAN;
+            } else {
+                textColor = Color.GRAY;
+                backgroundColor = Color.WHITE;
+            }
+
+            switchView.setTextColor(textColor);
+            switchView.setBackgroundColor(backgroundColor);
+            switchView.setTextSize(30);
+            switchView.setText(textOfLabel);
+
+            linearLayout.addView(switchView);
         }
     }
 
-    private void inflateListViewUsingDates() {
-        ListView listView = (ListView) findViewById(R.id.listview_count_zero_viewport);
-
-        EntryToListItemAdapter entryToListItemAdapter = new EntryToListItemAdapter(this, dates);
-        listView.setAdapter(entryToListItemAdapter);
-    }
-
-    //Called before the activity is paused.
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    protected void onResume() {
+        super.onResume();
 
-        saveIsActiveColumn();
+        loadFromSharedPreferences();
     }
 
-    private void saveIsActiveColumn() {
+    private void loadFromSharedPreferences() {
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearlayout_for_switches);
+        for (int day = START_OF_MONTH; day <= END_OF_MONTH; day++) {
+            int index = day - 1;
+            Switch switchView = (Switch) linearLayout.getChildAt(index);
+
+            String key = Integer.toString(day);
+            boolean isChecked = pref.getBoolean(key, false);
+
+            switchView.setChecked(isChecked);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        saveToSharedPreferences();
+    }
+
+    private void saveToSharedPreferences() {
         SharedPreferences pref = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
 
-        for (Entry entry : dates) {
-            String date = entry.getDate();
-            boolean isActive = entry.isActive();
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearlayout_for_switches);
+        for (int day = START_OF_MONTH; day <= END_OF_MONTH; day++) {
+            int index = day - 1;
+            Switch switchView = (Switch) linearLayout.getChildAt(index);
 
-            editor.putBoolean(date, isActive);
+            String key = Integer.toString(day);
+            boolean isChecked = switchView.isChecked();
+
+            editor.putBoolean(key, isChecked);
         }
 
-        ////////////////
         editor.commit();
-        ////////////////
-    }
-
-    private void loadIsActiveColumn() {
-        SharedPreferences pref = getPreferences(MODE_PRIVATE);
-
-        for (Entry entry : dates) {
-            String date = entry.getDate();
-            boolean isActive = pref.getBoolean(date, false);
-
-            entry.setActive(isActive);
-        }
     }
 
 }
