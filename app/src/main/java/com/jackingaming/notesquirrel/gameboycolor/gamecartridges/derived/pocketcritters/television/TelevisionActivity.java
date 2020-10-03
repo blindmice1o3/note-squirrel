b205c4c6ef0ffa0c685551c9e88c00a856a02043
jamesgov <blindmice1o3@gmail.com>
2020-10-03 15:29:15 -0700

    tinker: Use VideoView to play a file from "raw" directory
    When in TelevisionActivity, clicking the screen will launch the new VideoViewActivity and play a video file from the project's "raw" directory.

--- app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/gamecartridges/derived/pocketcritters/television/TelevisionActivity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/gameboycolor/gamecartridges/derived/pocketcritters/television/TelevisionActivity.java
@@ -1,8 +1,11 @@
 package com.jackingaming.notesquirrel.gameboycolor.gamecartridges.derived.pocketcritters.television;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.jackingaming.notesquirrel.R;
 
@@ -12,6 +15,15 @@ public class TelevisionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_television);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.constraintlayout_television);
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent videoViewActivityIntent = new Intent(TelevisionActivity.this, VideoViewActivity.class);
+                startActivity(videoViewActivityIntent);
+            }
+        });
     }
 
 }
