6d9a83df0f451a5aabdc21aceb6cbf8e820b4ded
jamesgov <blindmice1o3@gmail.com>
2021-01-24 22:19:56 -0800

    tinker: Change SpriteSheetVerifier2Activity to use RecyclerView

---
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/spritesheetverifier2/AdapterBitmapRecyclerView.java
@@ -0,0 +1,66 @@
+package com.jackingaming.notesquirrel.sandbox.spritesheetverifier2;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdapterBitmapRecyclerView extends RecyclerView.Adapter<AdapterBitmapRecyclerView.BitmapViewHolder> {
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+    private ItemClickListener itemClickListener;
+
+    private Context context;
+    private List<Bitmap> dataSource;
+
+    class BitmapViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        public ImageView imageView;
+
+        public BitmapViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView;
+            imageView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (itemClickListener != null) {
+                itemClickListener.onItemClick(v, getAdapterPosition());
+            }
+        }
+    }
+
+    public AdapterBitmapRecyclerView(Context context, List<Bitmap> dataSource, ItemClickListener itemClickListener) {
+        this.context = context;
+        this.dataSource = dataSource;
+        this.itemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public BitmapViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ImageView imageView = new ImageView(context);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        BitmapViewHolder bitmapViewHolder = new BitmapViewHolder(imageView);
+        return bitmapViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BitmapViewHolder holder, int position) {
+        holder.imageView.setImageBitmap(dataSource.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.size();
+    }
+}
--- app/src/main/java/com/jackingaming/notesquirrel/sandbox/spritesheetverifier2/SpriteSheetVerifier2Activity.java
+++ app/src/main/java/com/jackingaming/notesquirrel/sandbox/spritesheetverifier2/SpriteSheetVerifier2Activity.java
@@ -1,6 +1,8 @@
 package com.jackingaming.notesquirrel.sandbox.spritesheetverifier2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,14 +11,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.jackingaming.notesquirrel.MainActivity;
 import com.jackingaming.notesquirrel.R;
 
-public class SpriteSheetVerifier2Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private int mIndexColumn;
-    private int mIndexRow;
+public class SpriteSheetVerifier2Activity extends AppCompatActivity {
 
     private Bitmap[][] mRobotRSeries;
 
@@ -26,14 +29,10 @@ public class SpriteSheetVerifier2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_sprite_sheet_verifier2);
         Log.d(MainActivity.DEBUG_TAG, "SpriteSheetVerifier2Activity.onCreate(Bundle)");
 
-        final ImageView imageView = (ImageView) findViewById(R.id.iv_sprite_sheet_verifier2);
-        Button buttonColumnIncrement = (Button) findViewById(R.id.button_column_increment);
-        Button buttonColumnDecrement = (Button) findViewById(R.id.button_column_decrement);
-        Button buttonRowIncrement = (Button) findViewById(R.id.button_row_increment);
-        Button buttonRowDecrement = (Button) findViewById(R.id.button_row_decrement);
-
         //Initialize mRobotRSeries (crop and store the top portion of R.drawable.snes_chrono_trigger_r_series)
-        this.mRobotRSeries = new Bitmap[22][11];
+        int rows = 22;
+        final int columns = 11;
+        mRobotRSeries = new Bitmap[rows][columns];
         Bitmap spriteSheet = BitmapFactory.decodeResource(getResources(), R.drawable.snes_chrono_trigger_r_series);
         int widthSprite = 32;
         int heightSprite = 32;
@@ -47,81 +46,26 @@ public class SpriteSheetVerifier2Activity extends AppCompatActivity {
                         widthSprite, heightSprite);
             }
         }
-        //Might be redundant since local variable.
-        ///////////////////
-        spriteSheet = null;
-        ///////////////////
 
-        //Set the ImageView to the first cropped sprite.
-        this.mIndexColumn = 0;
-        this.mIndexRow = 0;
-        /////////////////////////////////////////////////////////////////
-        imageView.setImageBitmap(mRobotRSeries[mIndexRow][mIndexColumn]);
-        /////////////////////////////////////////////////////////////////
-
-        buttonColumnIncrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(MainActivity.DEBUG_TAG, "SpriteSheetVerifier2Activity OnClickListener button column increment");
-                //TODO:
-                mIndexColumn++;
-                if (mIndexColumn >= mRobotRSeries[mIndexRow].length) {
-                    mIndexColumn = mRobotRSeries[mIndexRow].length-1;
-                }
-
-                /////////////////////////////////////////////////////////////////
-                imageView.setImageBitmap(mRobotRSeries[mIndexRow][mIndexColumn]);
-                /////////////////////////////////////////////////////////////////
+        List<Bitmap> dataSource = new ArrayList<>();
+        for (int row = 0; row < mRobotRSeries.length; row++) {
+            for (int column = 0; column < mRobotRSeries[row].length; column++) {
+                dataSource.add(mRobotRSeries[row][column]);
             }
-        });
+        }
 
-        buttonColumnDecrement.setOnClickListener(new View.OnClickListener() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_sprite_sheet_verifier2);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, columns));
+        AdapterBitmapRecyclerView.ItemClickListener itemClickListener = new AdapterBitmapRecyclerView.ItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Log.d(MainActivity.DEBUG_TAG, "SpriteSheetVerifier2Activity OnClickListener button column decrement");
-                //TODO:
-                mIndexColumn--;
-                if (mIndexColumn < 0) {
-                    mIndexColumn = 0;
-                }
-
-                /////////////////////////////////////////////////////////////////
-                imageView.setImageBitmap(mRobotRSeries[mIndexRow][mIndexColumn]);
-                /////////////////////////////////////////////////////////////////
+            public void onItemClick(View view, int position) {
+                int row = position / columns;
+                int column = position % columns;
+                Toast.makeText(SpriteSheetVerifier2Activity.this, row + ", " + column, Toast.LENGTH_SHORT).show();
             }
-        });
-
-        buttonRowIncrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(MainActivity.DEBUG_TAG, "SpriteSheetVerifier2Activity OnClickListener button row increment");
-                //TODO:
-                mIndexRow++;
-                if (mIndexRow >= mRobotRSeries.length) {
-                    mIndexRow = mRobotRSeries.length-1;
-                }
-
-                /////////////////////////////////////////////////////////////////
-                imageView.setImageBitmap(mRobotRSeries[mIndexRow][mIndexColumn]);
-                /////////////////////////////////////////////////////////////////
-            }
-        });
-
-        buttonRowDecrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(MainActivity.DEBUG_TAG, "SpriteSheetVerifier2Activity OnClickListener button row decrement");
-                //TODO:
-                mIndexRow--;
-                if (mIndexRow < 0) {
-                    mIndexRow = 0;
-                }
-
-                /////////////////////////////////////////////////////////////////
-                imageView.setImageBitmap(mRobotRSeries[mIndexRow][mIndexColumn]);
-                /////////////////////////////////////////////////////////////////
-            }
-        });
+        };
+        AdapterBitmapRecyclerView adapterBitmapRecyclerView = new AdapterBitmapRecyclerView(this, dataSource, itemClickListener);
+        recyclerView.setAdapter(adapterBitmapRecyclerView);
     }
 
     @Override
